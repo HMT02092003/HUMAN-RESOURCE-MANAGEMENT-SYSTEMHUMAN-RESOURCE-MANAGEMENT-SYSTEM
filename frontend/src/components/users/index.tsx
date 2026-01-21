6728aefff18fc0e0577166339b1f2094aa9df9d1
DESKTOP-4D808T7\Toan <hoang.manh.toan@mqsolutions.com.vn>
2026-01-21 18:00:00 +0700

    test cicd

--- frontend/src/components/users/index.tsx
+++ frontend/src/components/users/index.tsx
@@ -37,6 +37,7 @@ const getStatusText = (statusCode: number | string): string => {
   return status ? status.label : '';
 };
 
+
 // Excel column configuration for user table
 const excelColumns: ExcelColumn[] = [
   {
